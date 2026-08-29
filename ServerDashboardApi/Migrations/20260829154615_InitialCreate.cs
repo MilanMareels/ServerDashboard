a056c35cd5cda53491d9b3a3f3d6412de90b85a0
MilanMareels <milan.mareels@gmail.com>
2026-08-29 17:47:55 +0200

    fix mirgation

---
+++ ServerDashboardApi/Migrations/20260829154615_InitialCreate.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerDashboardApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temp = table.Column<int>(type: "int", nullable: false),
+                    Severity = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProxmoxNodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RamGb = table.Column<int>(type: "int", nullable: false),
+                    Cores = table.Column<int>(type: "int", nullable: false),
+                    StorageGb = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProxmoxNodes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tempertures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temp = table.Column<int>(type: "int", nullable: false),
+                    BackFans = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TopAndBottomFans = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tempertures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VirtualMachines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RamGb = table.Column<int>(type: "int", nullable: false),
+                    Cores = table.Column<int>(type: "int", nullable: false),
+                    StorageGb = table.Column<double>(type: "float", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProxmoxNodeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VirtualMachines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VirtualMachines_ProxmoxNodes_ProxmoxNodeId",
+                        column: x => x.ProxmoxNodeId,
+                        principalTable: "ProxmoxNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VirtualMachines_ProxmoxNodeId",
+                table: "VirtualMachines",
+                column: "ProxmoxNodeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Tempertures");
+
+            migrationBuilder.DropTable(
+                name: "VirtualMachines");
+
+            migrationBuilder.DropTable(
+                name: "ProxmoxNodes");
+        }
+    }
+}
