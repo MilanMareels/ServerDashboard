0fee9a756a05a910684bd3ec91eab788da16348c
MilanMareels <milan.mareels@gmail.com>
2026-09-04 18:46:18 +0200

    add miggration.

--- ServerDashboardApi/Migrations/20260904164553_changeInModels.Designer.cs
+++ ServerDashboardApi/Migrations/20260904164553_changeInModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServerDashboardApi.Context;
 
@@ -11,9 +12,11 @@ using ServerDashboardApi.Context;
 namespace ServerDashboardApi.Migrations
 {
     [DbContext(typeof(DashBoardContext))]
-    partial class DashBoardContextModelSnapshot : ModelSnapshot
+    [Migration("20260904164553_changeInModels")]
+    partial class changeInModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ServerDashboardApi/Migrations/20260904164553_changeInModels.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerDashboardApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeInModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TopAndBottomFans",
+                table: "Tempertures",
+                newName: "TopFans");
+
+            migrationBuilder.RenameColumn(
+                name: "BackFans",
+                table: "Tempertures",
+                newName: "BottomFans");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TopFans",
+                table: "Tempertures",
+                newName: "TopAndBottomFans");
+
+            migrationBuilder.RenameColumn(
+                name: "BottomFans",
+                table: "Tempertures",
+                newName: "BackFans");
+        }
+    }
+}
